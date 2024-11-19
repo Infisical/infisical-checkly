53934fa00cc264e4976ed674144d75edcc8d74ad
Scott Wilson <scottraywilson@gmail.com>
2024-11-19 14:43:59 -0800

    test deploy and updated test workflow

--- src/__checks__/secrets/secrets-group.check.ts
+++ src/__checks__/secrets/secrets-group.check.ts
@@ -3,7 +3,7 @@ import { pagerDutyChannel } from "../../alert-channels";
 import { getLocations } from "../../locations";
 
 export const secretsGroup = new CheckGroup(`secrets-group`, {
-  name: `Secrets Group Check (${process.env.INSTANCE_ID?.toUpperCase()})`,
+  name: `Secrets Group Check [${process.env.INSTANCE_ID?.toUpperCase()}]`,
   activated: true,
   muted: false,
   concurrency: 50,
