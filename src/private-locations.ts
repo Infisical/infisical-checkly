bf61bf1c9db63df874f1d030a15195ea1b3d2dd2
Scott Wilson <scottraywilson@gmail.com>
2024-11-19 17:34:41 -0800

    add exclamation

--- src/private-locations.ts
+++ src/private-locations.ts
@@ -2,8 +2,8 @@ import { PrivateLocation } from "checkly/constructs";
 
 // Currently we only run against gamma as a PR status check
 
-export const privateLocation = new PrivateLocation(`infisical-${process.env.INSTANCE_ID}`, {
-  name: `Infisical ${process.env.INSTANCE_ID.toUpperCase()}`,
+export const privateLocation = new PrivateLocation(`infisical-${process.env.INSTANCE_ID!}`, {
+  name: `Infisical ${process.env.INSTANCE_ID!.toUpperCase()}`,
   icon: "squirrel",
-  slugName: `infisical-${process.env.INSTANCE_ID}`
+  slugName: `infisical-${process.env.INSTANCE_ID!}`
 });
