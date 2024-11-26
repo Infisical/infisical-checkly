b8b7df885166e36a0a2a6e5a404794d438c33e18
Scott Wilson <scottraywilson@gmail.com>
2024-11-25 18:29:58 -0800

    chore: move dashboard to locations... lol

--- src/locations.ts
+++ src/locations.ts
@@ -1,6 +1,14 @@
 import { privateLocation } from "./private-locations";
 import { Instance } from "./global-config";
 import { CheckConfigDefaults } from "checkly/dist/services/checkly-config-loader";
+import { Dashboard } from "checkly/constructs";
+
+new Dashboard(`infisical-${process.env.INSTANCE_ID!}`, {
+  header: `Infisical - ${process.env.INSTANCE_ID!.toUpperCase()}`,
+  description: `service availability and response times for Infisical ${process.env.INSTANCE_ID!.toUpperCase()}`,
+  tags: [process.env.INSTANCE_ID!],
+  customUrl: `infisical-${process.env.INSTANCE_ID!}`
+});
 
 export const getLocations = (): Pick<CheckConfigDefaults, "locations" | "privateLocations"> => {
   switch (process.env.INSTANCE_ID) {
