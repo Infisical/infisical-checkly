4001f64295eb230a515c13b753dd97833376fa25
Scott Wilson <scottraywilson@gmail.com>
2024-11-25 18:00:45 -0800

    test: try export dashboard

--- src/dashboards.ts
+++ src/dashboards.ts
@@ -1,6 +1,6 @@
 import { Dashboard } from "checkly/constructs";
 
-new Dashboard(`infisical-${process.env.INSTANCE_ID!}`, {
+export const dashboard = new Dashboard(`infisical-${process.env.INSTANCE_ID!}`, {
   header: `Infisical - ${process.env.INSTANCE_ID!.toUpperCase()}`,
   description: `service availability and response times for ${process.env.INSTANCE_ID!.toUpperCase()}`,
   tags: [process.env.INSTANCE_ID!],
