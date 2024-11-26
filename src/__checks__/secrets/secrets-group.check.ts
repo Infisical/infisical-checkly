f6654cde1e1fa55b49a075d3ef8a3e1119784b77
Scott Wilson <scottraywilson@gmail.com>
2024-11-25 18:24:02 -0800

    test: add to export file

--- src/__checks__/secrets/secrets-group.check.ts
+++ src/__checks__/secrets/secrets-group.check.ts
@@ -1,6 +1,8 @@
 import { CheckGroup } from "checkly/constructs";
 import { pagerDutyChannel } from "../../alert-channels";
 import { getLocations } from "../../locations";
+import { v4 as uuidv4 } from "uuid";
+import { Dashboard } from "checkly/constructs";
 
 export const secretsGroup = new CheckGroup(`secrets-group`, {
   name: `Secrets Group Check [${process.env.INSTANCE_ID?.toUpperCase()}]`,
@@ -10,4 +12,15 @@ export const secretsGroup = new CheckGroup(`secrets-group`, {
   runParallel: true,
   alertChannels: [pagerDutyChannel],
   ...getLocations()
+});
+
+new Dashboard("acme-dashboard-1", {
+  header: "ACME production",
+  description: "service availability and response times",
+  tags: ["prod", "api"],
+  logo: "https://assets.acme.com/images/acme-logo.png",
+  customUrl: `status-test-cli-${uuidv4()}`
+  // customCSS: {
+  //   entrypoint: path.join(__dirname, "dashboard.css")
+  // }
 });
